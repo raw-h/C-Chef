4c088297ed52097fcba2cb62fbd7112c051cde02
raw-h <rawat.shubham2400@gmail.com>
2022-08-24 18:50:33 +0530

    Day 6 Que 7(modified)

--- Array/7.cpp
+++ Array/7.cpp
@@ -1,51 +1,26 @@
 /*
+Chef is fan of pairs and he likes all things that come in pairs. He even has a doll collection in which the dolls come in pairs. One day while going through his collection he found that there are odd number of dolls. Someone had stolen a doll!!!
+
+Help chef find which type of doll is missing..
+*/
 #include <iostream>
 using namespace std;
 
 int main() {
 	int t;
-	cin >> t;
-	for (int i = 0; i < t; i++) {
-	    int n, k;
-	    cin >> n >> k;
+	cin>>t;
+	for(int i=0;i<t;i++){
+	    int n;
+	    cin>>n;
 	    int arr[n];
-	    for (int j = 0; j < n; j++) {
-	        cin >> arr[j];
+	    for(int i=0;i<n;i++){
+	       cin>>arr[i];
 	    }
-	    int count = 0;
-	    for (int j = 0; j < n; j++) {
-	        if(arr[j] > k)
-	            count++;
+	    int ans=arr[0];
+	    for(int i=1;i<n;i++){
+	        ans=(ans^arr[i]);
 	    }
-	    cout << count << endl;
+	    cout<<ans<<"\n";
 	}
 	return 0;
-}
-
-*/
-#include <iostream>
-using namespace std;
-
-int main()
-{
-    int t;
-    cin >> t;
-    for (int i = 0; i < t; i++)
-    {
-        int n, k;
-        cin >> n >> k;
-        int arr[n];
-        for (int j = 0; j < n; j++)
-        {
-            cin >> arr[j];
-        }
-        int count = 0;
-        for (int j = 0; j < n; j++)
-        {
-            if (arr[j] > k)
-                count++;
-        }
-        cout << count << endl;
-    }
-    return 0;
 }
