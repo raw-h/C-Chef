86d06c74d69ab9e151cd6c570ffca1ddd6e4951e
raw-h <rawat.shubham2400@gmail.com>
2022-08-26 12:26:46 +0530

    Day 6 Que 10(Updated)

--- Array/10.cpp
+++ Array/10.cpp
@@ -1,3 +1,11 @@
+/*
+Vishal loves running. He often visits his favourite Nehru Park and runs for very long distances. On one such visit he found that the number of girls in the park was unusually high. Now he wants to use this as an opportunity to impress a large number of girls with his awesome speed.
+
+The track on which he runs is an \text{N}N kilometres long straight path. There are \mathbf{a_i}a 
+i
+​
+  girls standing within the \text{ith}ith kilometre of this path. A girl will be impressed only if Vishal is running at his maximum speed when he passes by her. But he can run at his best speed only for a single continuous stretch of \text{K}K kilometres. Now Vishal wants to know what is the maximum number of girls that he can impress.
+*/
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -15,10 +23,18 @@ int main()
             cin >> arr[j];
         }
         int max_girls = INT_MIN;
-        for (int k = 0; k < length - 1; k++)
+        int sum = 0;
+        for (int k = 0; k < length; k++)
         {
-            int sum = arr[k] + arr[k + 1];
-            max_girls = max(max_girls, sum);
+            if ((k + max_dist) <= length)
+            {
+                for (int l = k; l < k + max_dist; l++)
+                {
+                    sum += arr[l];
+                    max_girls = max(max_girls, sum);
+                }
+            }
+            sum = 0;
         }
         cout << max_girls << endl;
     }
