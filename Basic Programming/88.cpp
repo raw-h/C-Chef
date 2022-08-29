9ac40ed601621c65a8e04ed7b23152fb0e4e87f8
raw-h <rawat.shubham2400@gmail.com>
2022-08-29 22:27:47 +0530

    Que 3 Corrected

--- Basic Programming/88.cpp
+++ Basic Programming/88.cpp
@@ -1,3 +1,8 @@
+/*
+Bears love candies and games involving eating them. Limak and Bob play the following game. Limak eats 1 candy, then Bob eats 2 candies, then Limak eats 3 candies, then Bob eats 4 candies, and so on. Once someone can't eat what he is supposed to eat, he loses.
+
+Limak can eat at most A candies in total (otherwise he would become sick), while Bob can eat at most B candies in total. Who will win the game? Print "Limak" or "Bob" accordingly.
+*/
 #include <iostream>
 using namespace std;
 
@@ -13,16 +18,13 @@ int main()
         for (int j = 0; j <= 1000; j++)
         {
             n2 = n1 + 1;
-            cout << "n2 "<< n2 << endl;
             if(bob < n2){
                 flag2 = 1;
                 break;
             }
             limak -= n1;
             bob -= n2;
-            n1++;
-            cout << n1 << endl;
-            cout << "Limak " << limak << endl;
+            n1 = n1 + 2;
             if(limak < n1){
                 flag1 = 1;
                 break;
@@ -30,7 +32,7 @@ int main()
         }
         if(flag1)
             cout << "Bob" << endl;
-        else
+        else if(flag2)
             cout << "Limak" << endl;
     }
     return 0;
