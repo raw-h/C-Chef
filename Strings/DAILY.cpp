1a9790671c1fd13a02d08e3b9b0e30e9071bb519
raw-h <rawat.shubham2400@gmail.com>
2022-11-18 21:07:00 +0530

    DAILY updated

--- Strings/DAILY.cpp
+++ Strings/DAILY.cpp
@@ -7,42 +7,25 @@ A group of X friends wants to buy tickets for free places, all of which are in o
 */
 #include <iostream>
 using namespace std;
-
 int fact(int t)
 {
     if (t == 0)
-    {
         return 1;
-    }
     if (t == 1)
-    {
         return 1;
-    }
     if (t == 2)
-    {
         return 2;
-    }
     if (t == 3)
-    {
         return 6;
-    }
     if (t == 4)
-    {
         return 24;
-    }
     if (t == 5)
-    {
         return 120;
-    }
     if (t == 6)
-    {
         return 720;
-    }
 }
-
 int main()
 {
-    // your code goes here
     int x, n;
     cin >> x >> n;
     long long int c = 0;
@@ -50,38 +33,25 @@ int main()
     {
         string s;
         cin >> s;
-
         for (int i = 0; i < 9; i++)
         {
             int count = 0;
             if (s[4 * i] == '0')
-            {
                 count++;
-            }
             if (s[4 * i + 1] == '0')
-            {
                 count++;
-            }
             if (s[4 * i + 2] == '0')
-            {
                 count++;
-            }
             if (s[4 * i + 3] == '0')
-            {
                 count++;
-            }
             if (s[53 - (2 * i)] == '0')
-            {
                 count++;
-            }
             if (s[53 - (2 * i + 1)] == '0')
-            {
                 count++;
-            }
             if (count >= x)
             {
                 long long int y = fact(count) / (fact(x) * fact(count - x));
-                c = c + y;
+                c += y;
             }
         }
     }
