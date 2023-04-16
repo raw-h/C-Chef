a89c5b13456bef95fc221b92b04ec2c4ddb58359
raw-h <rawat.shubham2400@gmail.com>
2023-04-16 21:52:18 +0530

    Q12 incomplete

---
+++ DSAPREP_01/ASTHIT.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        long long int n;
+        cin >> n;
+        vector<int>coord;
+        vector<int>size;
+        for(int i = 0; i < n; i++){
+            int x;
+            cin >> x;
+            coord.push_back(x);
+            int y;
+            cin >> y;
+            size.push_back(y);
+        }
+        
+    }
+    return 0;
+}
