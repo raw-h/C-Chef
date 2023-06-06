87e754ee31de55fce86d6a8e9a37846cb59d5f81
raw-h <rawat.shubham2400@gmail.com>
2023-06-06 10:52:13 +0530

    ASTHIT

--- DSAPREP_01/ASTHIT.cpp
+++ DSAPREP_01/ASTHIT.cpp
@@ -1,21 +1,74 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
+
+int main()
+{
     int t;
     cin >> t;
-    while(t--){
-        long long int n;
+
+    while (t-- > 0)
+    {
+        int n;
         cin >> n;
-        vector<int>coord;
-        vector<int>size;
-        for(int i = 0; i < n; i++){
-            int x;
-            cin >> x;
-            coord.push_back(x);
-            int y;
-            cin >> y;
-            size.push_back(y);
+
+        vector<int> dir(n);
+        vector<long long> power(n);
+
+        for (int i = 0; i < n; i++)
+        {
+            cin >> dir[i];
+            cin >> power[i];
         }
+
+        stack<int> asteroidStack;
+        vector<int> result;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (dir[i] == 1)
+            {
+                asteroidStack.push(i);
+            }
+            else
+            {
+                while (!asteroidStack.empty() && power[asteroidStack.top()] < power[i])
+                {
+                    power[i] += power[asteroidStack.top()];
+                    asteroidStack.pop();
+                }
+
+                if (!asteroidStack.empty())
+                {
+                    if (power[asteroidStack.top()] == power[i])
+                    {
+                        asteroidStack.pop();
+                    }
+                    else
+                    {
+                        power[asteroidStack.top()] += power[i];
+                    }
+                }
+                else
+                {
+                    result.push_back(i);
+                }
+            }
+        }
+
+        while (!asteroidStack.empty())
+        {
+            result.push_back(asteroidStack.top());
+            asteroidStack.pop();
+        }
+
+        sort(result.begin(), result.end());
+
+        cout << result.size() << endl;
+        for (int i : result)
+        {
+            cout << i + 1 << " ";
+        }
+        cout << endl;
     }
     return 0;
 }
